13f651b23db17f64a13252fc77e612ca250ee973
kgun90 <kgun90@gmail.com>
2020-09-14 10:57:51 +0900

    Initial Commit

---
+++ MEMOment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEMOment
+//
+//  Created by Geon Kang on 2020/09/14.
+//  Copyright © 2020 Geon Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
